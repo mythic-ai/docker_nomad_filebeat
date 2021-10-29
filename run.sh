1efbf892aa360f18d1c03c4cabfe083050866cb5
Grant Campbell <gcampbell@Mythics-MacBook-Pro.local>
2021-10-29 15:12:18 -0500

    added SIGINT catching

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
+# We need to capture these signals we we gracefully exit 
+# Without missing logs 
+function control_c {
+  echo "[!] waiting 45 seconds to finish writing logs before exiting..."
+  sleep 45
+  exit 
+}
+trap control_c SIGINT
+trap control_c SIGTERM
+
 for v in $(env | grep ^NOMAD_META_ | cut -d= -f1); do
   if [ -n "$meta_vars" ]; then
     meta_vars="${meta_vars},${v}"
